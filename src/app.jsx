2ec389e4f862128e2be86deb41d254bc113d7e30
Eleonora Kazakova <abei-2005@yandex.ru>
2021-05-03 21:01:55 +0200

    Create layout

---
+++ src/app.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './styles/styles';
+
+export default function App() {
+
+  return(
+    <div className='grid'>
+      <div className='header'></div>
+      <div className='menu'></div>
+      <div className='content'></div>
+    </div>
+  )
+}
