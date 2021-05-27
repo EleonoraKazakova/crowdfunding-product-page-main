15cdf121ce414fe289fb92c8ee41fbd38701a843
Eleonora Kazakova <abei-2005@yandex.ru>
2021-05-27 20:57:15 +0200

    Pledge with no reward

--- src/dataReward.jsx
+++ src/dataReward.jsx
@@ -4,11 +4,23 @@ import Reward from './reward'
 
 export default function DataReward({ modal, close, addAmount, amountLeft, setAmountLeft }) {
 
-  const [selectedReward, setSelectedReward] = useState(0)
+  const [selectedReward, setSelectedReward] = useState(-1)
 
   return (
     <div className='dataReward-rewards'>
-
+      {modal ? <Reward
+        title='Pledge with no reward'
+        minPledge={0}
+        text='Aliquam erat volutpat. Pellentesque commodo, eros a
+              pharetra pharetra, ipsum mi convallis erat, non cursus ex augue
+              rhoncus mauris.'
+        left={null}
+        modal={modal}
+        toggle={() => setSelectedReward(0)}
+        selected={selectedReward === 0}
+        close={close}
+        addAmount={(pledge) => { addAmount(pledge); }}
+      /> : null}
       <Reward
         title='Bamboo Stand'
         minPledge={25}
@@ -20,7 +32,7 @@ export default function DataReward({ modal, close, addAmount, amountLeft, setAmo
         toggle={() => setSelectedReward(1)}
         selected={selectedReward === 1}
         close={close}
-        addAmount={ (pledge) => {addAmount(pledge); setAmountLeft({...amountLeft, 25: amountLeft[25] - 1})} }
+        addAmount={(pledge) => { addAmount(pledge); setAmountLeft({ ...amountLeft, 25: amountLeft[25] - 1 }) }}
       />
 
       <Reward
@@ -34,7 +46,7 @@ export default function DataReward({ modal, close, addAmount, amountLeft, setAmo
         toggle={() => setSelectedReward(2)}
         selected={selectedReward === 2}
         close={close}
-        addAmount={ (pledge) => {addAmount(pledge); setAmountLeft({...amountLeft, 75: amountLeft[75] - 1})} }
+        addAmount={(pledge) => { addAmount(pledge); setAmountLeft({ ...amountLeft, 75: amountLeft[75] - 1 }) }}
       />
 
       <Reward
@@ -48,7 +60,7 @@ export default function DataReward({ modal, close, addAmount, amountLeft, setAmo
         toggle={() => setSelectedReward(3)}
         selected={selectedReward === 3}
         close={close}
-        addAmount={ (pledge) => {addAmount(pledge); setAmountLeft({...amountLeft, 200: amountLeft[200] - 1})} }
+        addAmount={(pledge) => { addAmount(pledge); setAmountLeft({ ...amountLeft, 200: amountLeft[200] - 1 }) }}
       />
 
     </div>
